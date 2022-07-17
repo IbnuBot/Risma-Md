0cd21b2b88727b01fc4266d329e65ea67ca7559b
IbnuBot <101147082+IbnuBot@users.noreply.github.com>
2022-07-17 09:41:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -36,12 +36,12 @@ global.wait = '_*𝒔𝒆𝒅𝒂𝒏𝒈 𝒅𝒊 𝒑𝒓𝒐𝒔𝒆𝒔...*_
 global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓...*_'
 
 //========Url Template Buttons=========//
-global.dtu = '𝑌𝑜𝑢𝑡𝑢𝑏𝑒'
-global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.dtu = 'Youtube IbnuOfc'
+global.urlnya = "https://youtube.com/channel/Ibnu Official "
 
 //============= callButtons =============//
-global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '62 857-1313-7903'
+global.dtc = 'Owner IbnuOfc'
+global.phn = '62 895-3386-90940'
 
 //============= Games ================//
 global.benar = '_*Benar*_'
